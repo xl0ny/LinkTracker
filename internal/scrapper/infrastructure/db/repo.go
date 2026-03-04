e97ddf8495f7eee30be6b000701cb1f35bc0b5b2
Turskov Evgeny <turskov07081@gmail.com>
2026-03-05 01:10:10 +0300

    начал писать 2(пишу на 2-х устройствах (чтобы перкинуть вообщем))

--- internal/scrapper/infrastructure/db/repo.go
+++ internal/scrapper/infrastructure/db/repo.go
@@ -1,4 +1,22 @@
 package db
 
 type reposiotory struct {
+	Chats map[int]struct{}
+}
+
+func (r *reposiotory) NewRepository() *reposiotory {
+	return &reposiotory{}
+}
+
+func (r *reposiotory) AddChat(id int) error {
+	if !r.exists(id) {
+		r.Chats[id] = struct{}{}
+		return nil
+	}
+
+}
+
+func (r *reposiotory) exists(id int) bool {
+	_, ok := r.Chats[id]
+	return ok
 }
